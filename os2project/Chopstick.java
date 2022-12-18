b056029b7172997c3a3cc4b539d4d620b806f25d
Yomna-Abdelghany <116431762+Yomna-Abdelghany@users.noreply.github.com>
2022-12-18 22:59:24 +0200

    Add files via upload

---
+++ os2project/Chopstick.java
@@ -0,0 +1,17 @@
+package os2project;
+
+public class Chopstick {
+    private boolean availability;
+  
+    public Chopstick() {
+      availability = true;
+    }
+  
+    public boolean getAvailability() {
+      return availability;
+    }
+  
+    public void setAvailability(boolean flag) {
+      availability = flag;
+    }
+}
